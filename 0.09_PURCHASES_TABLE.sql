50691d8152dd0225f2a64cc4dd4775b1318c7a70
Josh Lohman <jjosh9708@gmail.com>
2017-11-20 15:15:33 -0500

    Purchase Tables added and connected to other tables

---
+++ 0.09_PURCHASES_TABLE.sql
@@ -0,0 +1,22 @@
+﻿USE SQL_Library;    
+GO    
+--Replace the X's with their proper names
+CREATE TABLE dbo.Purchases (
+    PurchaseID INT IDENTITY (1, 1) NOT NULL,
+    ProductID  INT NOT NULL,
+    CustomerID INT NOT NULL,
+    Quantity   INT NULL,
+    CONSTRAINT [PK_dbo.Purchases] PRIMARY KEY CLUSTERED ([PurchaseID] ASC),
+    CONSTRAINT [FK_dbo.Purchases_dbo.Customers_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Purchases_dbo.Products_ProductID] FOREIGN KEY ([ProdudtID]) REFERENCES [dbo].[Products] ([ProductID]) ON DELETE CASCADE
+);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ProductID]
+    ON [dbo].[Purchases]([ProductID] ASC);
+
+GO
+CREATE NONCLUSTERED INDEX [IX_CustomerID]
+    ON [dbo].[Purchases]([CustomerID] ASC);
+
+GO
